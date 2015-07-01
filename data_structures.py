e7db43928f486e80cb78f5e5712bbac85e8d5333
mtthss <matteo.hessel@mail.polimi.it>
2015-07-01 12:44:31 +0100

    Collection and document data structures

--- data_structures.py
+++ data_structures.py
@@ -1,6 +1,7 @@
-
 import os
 import xml.etree.ElementTree as ET
+import nltk
+
 
 __author__ = 'matteo'
 
@@ -8,6 +9,10 @@ __author__ = 'matteo'
 class Corpus:
 
     def __init__(self, input):
+        sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+        sent_detector.tokenize("  ".strip())
+
+        # pass tokenizer to string
         self.collection_list = []
 
     # read corpus from a specified directory
@@ -29,11 +34,14 @@ class Corpus:
 
 class Collection:
 
-    def __init__(self):
-        self.topic_title = -1       # keywords / topic title
-        self.topic_descr = -1       # description of expected content
-        self.raw_docs = {}          # documents to summarize: {id: document-object}
-        self.raw_references = {}    # human references: {id: raw-text}
+    def __init__(self, tokenizer=None):
+
+        self.sent_detector = tokenizer if tokenizer!=None else nltk.data.load('tokenizers/punkt/english.pickle')
+
+        self.topic_title = -1   # keywords / topic title
+        self.topic_descr = -1   # description of expected content
+        self.docs = {}          # documents to summarize: {id: document-object}
+        self.references = {}    # human references: {id: raw-text}
 
     def readCollectionFromDir(self, year, code):
 
@@ -54,7 +62,7 @@ class Collection:
         for filename in os.listdir(doc_path):
             root = ET.parse(doc_path+"/"+filename).getroot()
             id = root.find('DOCNO').text
-            self.raw_docs[id] = Document(root.find('HEADLINE').text, root.find('TEXT').text, self)
+            self.docs[id] = Document(root.find('HEADLINE').text, root.find('TEXT').text, self)
 
         # read references
         for filename in os.listdir(ref_path):
@@ -62,29 +70,66 @@ class Collection:
             with open(ref_path+"/"+filename, 'r') as f:
                 content = f.read()
             if encod[0].lower()==code[:-1]:
-                self.raw_references[encod[4]]=content
+                self.references[encod[4]]=content
 
 
 class Document:
 
-    def __init__(self, headline, raw_text, collection):
-        self.headline = headline    # headline of the document
-        self.raw_text = raw_text    # raw text of the document
-        self.father = collection    # reference to collection-object
-        self.sent = {}              # {sentence-position: (raw_text, features, rel-score)
+    def __init__(self, headline, raw_text, collection=None):
+        if collection==None: collection = Collection()
+        self.headline = headline                        # headline of the document
+        self.raw_text = raw_text.replace('\n', ' ')     # raw text of the document
+        self.father = collection                        # reference to collection-object
+        self.sent = {}                                  # {sentence-position: (raw_text, features, rel-score)
+
+    def process_score_document(self):
+        tokenized = self.father.sent_detector.tokenize(self.raw_text)
+        count = 0
+        for s in tokenized:
+            self.sent[count] = (s, self.compute_features(s, count), self.compute_svr_score(s), self.compute_ranksvm_score(s))
 
     def process_document(self):
+        tokenized = self.father.sent_detector.tokenize(self.raw_text)
+        count = 0
+        for s in tokenized:
+            self.sent[count] = (s, self.compute_features(s))
 
+    def compute_features(self, sentence, count):
+        return (count)
+
+    def compute_svr_score(self, sentence):
+        return 0
+
+    def compute_ranksvm_score(self, sentence):
+        return 0
+
+
+class Reference:
+
+    def __init__(self, raw_text, collection):
+
+        self.ref = raw_text.strip()
+        self.ref_sent = collection.sent_detector.tokenize(self.ref)
+
+    def ref_sent_sim(self, sentence):
         pass
 
 
 
 if __name__ == '__main__':
 
-    print "testing collection class..."
 
-    c = Collection()
+    print "\ntesting tokenizer..."
+    sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+
+    print "\ntesting collection class..."
+
+    c = Collection(sent_detector)
     c.readCollectionFromDir(2005,"d301i")
-
-    print c.topic_descr
     print c.topic_title
+
+    print "\ntesting document class..."
+
+    d = Document("my_title", "my_doc", c)
+    print d.father.topic_title
+    d.process_score_document()
