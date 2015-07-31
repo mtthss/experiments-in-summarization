f2d770c361e1c293c5ea8ca5a2bb61323045c846
mtthss <matteo.hessel@mail.polimi.it>
2015-07-31 16:46:23 +0100

    rouge preprocess added choice test collection

--- preprocess.py
+++ preprocess.py
@@ -1,16 +1,19 @@
 import os
 import nltk
 
+
 __author__ = 'matteo'
 
 
 ref = False
 rivals = True
-
+test_refs = ["d632i","d426a","D0628A","d324e","D0644H","D0601A","D0630C","D0647B","D0606F",
+             "D0637A","D0607G","d407b","d346h","d393f","d350a","d400b","d332h","D0602B","D0623E"]
 
 # references
 if ref:
-    year = 2006
+
+    year = 2005
     ref_in_path = "./data/references/"+str(year)
     ref_out_path = "./data2/references/"+str(year)+"/"
     sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
@@ -18,15 +21,19 @@ if ref:
     for filename in os.listdir(ref_in_path):
 
         content = open(ref_in_path+'/'+filename, 'r').read()
-
         code = filename.split(".")
-        new_name = (code[0]+code[3]+"_"+code[4]).lower()
-        out_file = open(ref_out_path+new_name, "w")
 
-        for s in sent_detector.tokenize(content):
-            out_file.write(s+"\n")
+        cond = code[0]+code[3] if year==2006 else (code[0]+code[3]).lower()
 
-        out_file.close()
+        if cond in test_refs:
+
+            new_name = (code[0]+code[3]+"_"+code[4]).lower()
+            out_file = open(ref_out_path+new_name, "w")
+
+            for s in sent_detector.tokenize(content):
+                out_file.write(s+"\n")
+
+            out_file.close()
 
 
 # other automatic summaries
@@ -34,15 +41,19 @@ if rivals:
 
     path_in = "./rivals/peers/"
     path_out = "./rivals2/peers/"
+    count = 0
+    test_refs_low = [x.lower() for x in test_refs]
 
     for filename in os.listdir(path_in):
 
         content = open(path_in+filename, 'r').read()
-
         code = filename.split(".")
-        new_name = (code[0]+code[3]+"_"+code[4]).lower()
-        out_file = open(path_out+new_name, "w")
 
-        out_file.write(content)
-        out_file.close()
+        if (code[0]+code[3]).lower() in test_refs:
+
+            new_name = (code[0]+code[3]+"_"+code[4]).lower()
+            out_file = open(path_out+new_name, "w")
+
+            out_file.write(content)
+            out_file.close()
 
